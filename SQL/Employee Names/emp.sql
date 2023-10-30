fdb50f7fe1ae81e7ffb47a2146e722f7d44738b0
Shravya-11 <146946500+Shravya-11@users.noreply.github.com>
2023-10-30 17:28:05 +0530

    Create emp.sql

---
+++ SQL/Employee Names/emp.sql
@@ -0,0 +1,5 @@
+// Employee names in ascending order
+INPUT:- Rose , Angela, Frank, Patrick, Lisa.
+  Query:-
+Select name from Employee ORDER BY name ASC;
+OUTPUT:-Angela, Frank, Lisa, Patrick, Rose.
